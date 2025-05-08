06182a5b101457c509fc48c92e78ad514463a287
Unkx <97525106+Unkx@users.noreply.github.com>
2025-05-08 18:52:56 +0200

    Delete Desktop directory

--- Desktop/TaskMasters-master/server/src/main/java/pl/server/Repositories/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package pl.server.Repositories;
-
-import pl.server.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByEmail(String email);
-}
