35624fd1be12716b7f899df90c5204694ddece4b
Unkx <97525106+Unkx@users.noreply.github.com>
2025-05-08 19:07:29 +0200

    Delete Desktop directory

--- Desktop/TaskMasters-Unkx-patch-1/server/src/main/java/pl/server/Repositories/OrganizationRepository.java
+++
@@ -1,7 +0,0 @@
-package pl.server.Repositories;
-
-import pl.server.Organization;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrganizationRepository extends JpaRepository<Organization, Long> {
-}
--- Desktop/TaskMasters-Unkx-patch-1/server/src/main/java/pl/server/Repositories/TaskRepository.java
+++
@@ -1,7 +0,0 @@
-package pl.server.Repositories;
-
-import pl.server.Task;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TaskRepository extends JpaRepository<Task, Long> {
-}
