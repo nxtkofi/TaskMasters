1edc619da9fda7148aee5bf9aa535667413a51c8
Fabian Szkudlarski <fabian.szkudlarski@gmail.com>
2025-05-15 01:38:10 +0200

    usuniecie folderu Backend

--- Backend/taskmanager/src/main/java/com/example/taskmanager/dto/AuthRequest.java
+++
@@ -1,9 +0,0 @@
-package com.example.taskmanager.dto;
-
-import lombok.Data;
-
-@Data
-public class AuthRequest {
-    private String email;
-    private String password;
-}
--- Backend/taskmanager/src/main/java/com/example/taskmanager/repository/TeamRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.taskmanager.repository;
-
-import com.example.taskmanager.entity.Team;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TeamRepository extends JpaRepository<Team, Long> {}
